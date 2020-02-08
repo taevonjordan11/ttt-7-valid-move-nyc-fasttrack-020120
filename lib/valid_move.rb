90e7bc0859722f1a6e342ee3534d83a449d8bab6
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-08 20:07:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,4 @@
+# code your #valid_move? method here
 
-  
-def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
