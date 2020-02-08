d6f1ee49fce63c45dd49db4ccf27a8c16e79375d
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-08 20:06:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++
@@ -1,9 +0,0 @@
-  
-def valid_move?(board, index)
-  def position_taken?(array, ind)
-    if array[ind] == " " || array[ind] == "" || array[ind] == nil
-      return false
-    else
-      return true
-    end
-  end
