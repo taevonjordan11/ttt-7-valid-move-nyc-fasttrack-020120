7482a5f708979d49643b1b7b36ca1f37cc5dc900
agile-proc-6339 <taevonjordan1@gmail.com>
2020-02-08 20:08:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,20 +7,3 @@ def valid_move?(board, index)
       return true
     end
   end
-
-  def on_board?(num)
-    if num.between?(0, 8) == true
-      return true
-    else
-      return false
-    end
-  end
-
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
-    return true
-  else
-    return false
-  end
-
-
-end
